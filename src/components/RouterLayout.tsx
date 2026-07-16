7fa18e3d6c409c55bc88af188978e2b3fbe8d62b
ioannispriovolos <priovolosg@hotmail.com>
2026-07-16 14:07:33 +0300

    chore: implemented router layout

---
+++ src/components/RouterLayout.tsx
@@ -0,0 +1,20 @@
+import Header from "./Header.tsx";
+import Footer from "./Footer.tsx";
+import {Outlet} from "react-router";
+import {Toaster} from "sonner";
+
+const RouterLayout = () => {
+    return (
+        <>
+            <div>
+                <Header/>
+                <main className="container mx-auto min-h-[95vh] pt-36">
+                    <Outlet />
+                </main>
+                <Footer/>
+                <Toaster />
+            </div>
+        </>
+    )
+}
+export default RouterLayout;
