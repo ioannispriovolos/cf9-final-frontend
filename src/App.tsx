3ac08be2601fa73965912c2405965d4f499c4d3b
ioannispriovolos <priovolosg@hotmail.com>
2026-07-16 14:06:56 +0300

    chore: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
+import {Route, Routes} from "react-router";
+import RouterLayout from "@/components/RouterLayout.tsx";
+import HomePage from "@/pages/HomePage.tsx";
+import LoginPage from "@/pages/LoginPage.tsx";
 
 
 function App() {
 
   return (
     <>
+      <Routes>
+        <Route element={<RouterLayout />}>
+          <Route index element={<HomePage />} />
+
+          <Route path="login" element={<LoginPage />} />/
+
+        </Route>
+
+        {/*<Route path="*" element={<NotFoundPage />} />*/}
+
+
+      </Routes>
     </>
   )
 }
