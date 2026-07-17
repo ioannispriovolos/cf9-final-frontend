ec6837827edd49b3452e428cb922d7e808ab041e
ioannispriovolos <priovolosg@hotmail.com>
2026-07-17 16:16:33 +0300

    chore: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import {Route, Routes} from "react-router";
 import RouterLayout from "@/components/RouterLayout.tsx";
 import HomePage from "@/pages/HomePage.tsx";
 import LoginPage from "@/pages/LoginPage.tsx";
+import RoleGuard from "@/components/RoleGuard.tsx";
+import AdminDashboardPage from "@/pages/AdminDashboardPage.tsx";
 
 
 function App() {
@@ -13,6 +15,21 @@ function App() {
           <Route index element={<HomePage />} />
 
           <Route path="login" element={<LoginPage />} />/
+
+            {/* 1. Protected Admin Spaces */}
+            <Route element={<RoleGuard allowedRoles={["ADMIN"]} />}>
+                <Route path="admin" element={<AdminDashboardPage />} />
+            </Route>
+
+            {/* 2. Protected Engineer Spaces */}
+            {/*<Route element={<RoleGuard allowedRoles={["NETWORK_ENGINEER"]} />}>*/}
+            {/*    <Route path="engineer/dashboard" element={<EngineerDashboard />} />*/}
+            {/*</Route>*/}
+
+            {/* 3. Protected Viewer Spaces */}
+            {/*<Route element={<RoleGuard allowedRoles={["VIEWER"]} />}>*/}
+            {/*    <Route path="viewer/dashboard" element={<ViewerDashboard />} />*/}
+            {/*</Route>*/}
 
         </Route>
 
